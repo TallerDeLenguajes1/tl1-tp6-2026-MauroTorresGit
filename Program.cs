a2359c85b512b61ce1125bb2a3d7be7b415e9f73
MauroTorresGit <maurotorres2802@gmail.com>
2026-05-25 18:24:34 -0300

    segundo commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*Console.WriteLine("Hello, World!");
+
+int a = 10; 
+int b = a;
+
+Console.WriteLine("Valor de a:" + a);
+Console.WriteLine("Valor de b:" + b);*/
+
+float numero = 0, numeroInvertido = 0;
+
+Console.WriteLine("Ingrese un numero:");
+string? input = Console.ReadLine();
+
+if (float.TryParse(input, out numero))
+{
+    if (numero > 0)
+    {
+        numeroInvertido = 1 / numero;
+        Console.WriteLine("El numero invertido de " + numero + " es: " + numeroInvertido);
+    } 
+    else
+    {
+        Console.WriteLine("El numero debe ser mayor a 0");   
+    }
+    
+} 
+else
+{
+    Console.WriteLine("No se ingreso un numero");    
+}
+
+
