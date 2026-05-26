0d34db9c9656a0c6c51fc91cb4859f7f7b0b4ca7
MauroTorresGit <maurotorres2802@gmail.com>
2026-05-26 18:51:48 -0300

    variable n

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿double numero1, valorAbs, seno, coseno, cuadrado, raiz;
+﻿double numero1, valorAbs, seno, coseno, cuadrado, raiz,n=0;
 
 /*Console.WriteLine("Ingrese una operacion:");
 Console.WriteLine("Ingrese una operacion:");
